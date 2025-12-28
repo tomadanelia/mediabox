c3fc5743976af437d04e8c78155aaf91978008f6
tomadanelia <tomadanelia16@gmail.com>
2025-12-28 20:04:18 +0400

    Add:balance table and model of it

---
+++ database/migrations/2025_12_28_155135_create_accounts_table.php
@@ -0,0 +1,22 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->uuid('id')->primary(); 
+            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade'); 
+            $table->decimal('balance', 15, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
