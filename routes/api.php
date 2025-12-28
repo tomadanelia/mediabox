7526cf63d1ee5ceab36e352eb5f5d12c6c3b8e7a
tomadanelia <tomadanelia16@gmail.com>
2025-12-28 04:22:06 +0400

    added throttle to rate limit requests

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChannelController; 
 use App\Http\Controllers\AuthController;
-Route::get('/channels', [ChannelController::class, 'index']);
-Route::post('/auth/register', [AuthController::class, 'register']);
-Route::post('/auth/verify', [AuthController::class, 'verify']);
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/channels', [ChannelController::class, 'index']) ->middleware('throttle:60,1');
+Route::post('/auth/login', [AuthController::class, 'login'])  ->middleware('throttle:3,1');
+Route::post('/auth/verify', [AuthController::class, 'verify']) ->middleware('throttle:3,1');
+Route::post('/auth/register', [AuthController::class, 'register']) ->middleware('throttle:5,1');
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     
