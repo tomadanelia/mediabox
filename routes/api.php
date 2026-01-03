a31c832532f2c981ac0a3e9ff1eef6dba90e1584
tomadanelia <tomadanelia16@gmail.com>
2026-01-03 14:04:25 +0400

    added IpWhiteList to let only interpay ip to access endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\InterPayController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChannelController; 
@@ -9,7 +9,7 @@ Route::post('/auth/login', [AuthController::class, 'login'])  ->middleware('thro
 Route::post('/auth/verify', [AuthController::class, 'verify']) ->middleware('throttle:3,1');
 Route::post('/auth/register', [AuthController::class, 'register']) ->middleware('throttle:5,1');
 Route::post('/auth/resend', [AuthController::class, 'resendCode'])->middleware('throttle:3,1');
-Route::get('/interpay/balance', [InterPayController::class, 'handle']);
+Route::post('/interpay/balance', [InterPayController::class, 'handle'])->middleware('whitelist.ip');
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     
