fcbbea79416d0a8fdcf27e620d3b66fcbc49879f
tomadanelia <tomadanelia16@gmail.com>
2026-01-10 06:22:18 +0400

    added channelcontroller and routes to get streams channels archives and programs

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChannelController; 
 use App\Http\Controllers\AuthController;
-Route::get('/channels', [ChannelController::class, 'index']) ->middleware('throttle:60,1');
+Route::prefix('channels')->group(function () {
+    Route::get('/', [ChannelController::class, 'index']);
+    Route::get('/{id}/stream', [ChannelController::class, 'stream']); 
+    Route::get('/{id}/programs', [ChannelController::class, 'programs']);
+    Route::get('/{id}/archive', [ChannelController::class, 'archive']);
+});
 Route::post('/auth/login', [AuthController::class, 'login'])  ->middleware('throttle:3,1');
 Route::post('/auth/verify', [AuthController::class, 'verify']) ->middleware('throttle:3,1');
 Route::post('/auth/register', [AuthController::class, 'register']) ->middleware('throttle:5,1');
