1f596c7841832763b313651823fc53aafec4348e
tomadanelia <tomadanelia16@gmail.com>
2026-02-24 19:47:01 +0400

    disabling csrf temporarily for specific route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
     $middleware->alias([
         'whitelist.ip' => IpWhiteList::class,
     ]);
+    $middleware->validateCsrfTokens(except: [
+    'api/admin/categories/*', //absolutely have ti change this in production, but for now we need to be able to test the admin category management without CSRF tokens
+    ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
