b1426de04bf6b20b944de9a052dfa9d4b15e9b25
tomadanelia <tomadanelia16@gmail.com>
2026-02-25 12:14:33 +0400

    bug fix: made correct import in file

--- app/Http/Controllers/AdminPlansController.php
+++ app/Http/Controllers/AdminPlansController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SubscriptionPlan;
+use Illuminate\Support\Facades\Cache;
 class AdminPlansController extends Controller
 {
     public function addPlan(Request $request)
