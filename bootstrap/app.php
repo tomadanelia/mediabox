1eee34abdb2ced4e1817e8ed289a7e285e6fea97
tomadanelia <tomadanelia16@gmail.com>
2026-03-16 18:20:37 +0400

    bug fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\IpWhiteList;
+use App\Http\Middleware\AdminMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();     
         $middleware->alias([
             'whitelist.ip' => IpWhiteList::class,
-            'admin'        => \App\Http\Middleware\AdminMiddleware::class,
+            'admin'        => AdminMiddleware::class,
         ]);
         $middleware->trustProxies(at: '*'); 
     });
