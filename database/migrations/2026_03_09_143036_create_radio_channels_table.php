2c7b6eea49124b8fe05bd1ac5086f0652a01025a
tomadanelia <tomadanelia16@gmail.com>
2026-04-18 15:55:19 +0400

    fix radio channels table

--- database/migrations/2026_03_09_143036_create_radio_channels_table.php
+++ database/migrations/2026_03_09_143036_create_radio_channels_table.php
@@ -9,14 +9,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('radio_channels', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('external_id')->unique()->index(); 
-            $table->string('name');
-            $table->string('icon_url')->nullable();
-            $table->boolean('is_active')->default(true);
-            $table->boolean('is_free')->default(false);
-            $table->timestamps();
-        });
+         $table->uuid('id')->primary();
+         $table->unsignedTinyInteger('external_id')->unique();   
+         $table->string('stream_url')->nullable(); 
+         $table->string('name');
+         $table->string('icon_url')->nullable();
+         $table->boolean('is_active')->default(true);
+         $table->boolean('is_free')->default(false);
+         $table->timestamps();
+});
 
         Schema::create('radio_subscription_plan', function (Blueprint $table) {
             $table->id();
