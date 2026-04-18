01294bac7de9aa1d4059c45ca5165dd89549fdc9
tomadanelia <tomadanelia16@gmail.com>
2026-04-18 15:22:17 +0400

    fix(favovurites table): updated migration to work right in mysql

--- database/migrations/2026_03_31_191722_add_device_id_field_in_user_favourites_table.php
+++ database/migrations/2026_03_31_191722_add_device_id_field_in_user_favourites_table.php
@@ -7,19 +7,32 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     public function up(): void
-{
-    Schema::table('user_favourites', function (Blueprint $table) {
-        $table->string('device_id')->default('spa_web')->after('user_id');
-        $table->dropUnique(['user_id', 'channel_id']);
-        $table->unique(['user_id', 'channel_id', 'device_id']);
-    });
-}
+    {
+        Schema::table('user_favourites', function (Blueprint $table) {
+            $table->string('device_id')->default('spa_web')->after('user_id');
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['channel_id']);
+            $table->dropUnique(['user_id', 'channel_id']);
+            $table->unique(['user_id', 'channel_id', 'device_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade');
+        });
+    }
+
     public function down(): void
-{
-    Schema::table('user_favourites', function (Blueprint $table) {
-        $table->dropUnique(['user_id', 'channel_id', 'device_id']);
-        $table->unique(['user_id', 'channel_id']);
-        $table->dropColumn('device_id');
-    });
-}
+    {
+        Schema::table('user_favourites', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['channel_id']);
+            
+            $table->dropUnique(['user_id', 'channel_id', 'device_id']);
+            
+            $table->unique(['user_id', 'channel_id']);
+            
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade');
+            
+            $table->dropColumn('device_id');
+        });
+    }
 };
